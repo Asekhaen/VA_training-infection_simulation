17b061f40aa9a744b99897fc50d4fee2cdb32153
SeniorK <senior.k@unimelb.edu.au>
2025-02-21 09:16:59 +0800

    Full simulation script with new plotting code

--- R/simulation_walkthrough.R
+++ R/simulation_walkthrough.R
@@ -45,6 +45,149 @@ expected_inf_daily <- true_inf_inc * pop
 plot(expected_inf_daily~days) #plot to visualise
 
 
+#### Set up structure for data simulations ####
+# Simulate number of new infections per day 
+#(across the population, therefore new infection = new infected agent)
+inf_daily <- rnbinom(n_days, mu = expected_inf_daily, size = 100)
+
+plot(inf_daily ~ days, type = "l", lty = "dotted") #plot to visualise
+abline(v = prev_days, #add infection prevalence surveys to the plot
+       lty = 2)
+
+# create an empty dataframe the length of the disease outbreak 
+# to store case timeseries we will be simulating
+clinical_cases <- tibble( 
+  day = days,
+  cases = NA
+)
+
+prevalence_surveys <- tibble( #and prevalence surveys
+  day = prev_days,
+  n_sampled = NA,
+  n_positive = NA
+)
+
+agents <- tibble( #and specific information for each agent
+  infection_day = rep(NA, pop),
+  ever_symptomatic = rep(NA, pop),
+  symptom_delay = rep(NA, pop)
+)
+#set our seed
+set.seed(2025-02-20)
+
+#### Now simulate ####
+# Specifically, clinical case timeseries and prevalence surveys with an
+# agent-based model, using a for-loop
+for (day in days) { #this will run the below code across all days in our 365 day sequence
+  
+  # generate new infected agents
+  n_new_infections <- inf_daily[day]
+  
+  new_infected_agents <- tibble(
+    infection_day = rep(NA, n_new_infections),
+    ever_symptomatic = rep(NA, n_new_infections),
+    symptom_delay = rep(NA, n_new_infections)
+  )
+  
+  for (this_infected_agent in seq_len(n_new_infections)) {
+    
+    # determine for each one whether they will ever be symptomatic
+    this_agent_ever_symptomatic <- rbinom(1, 1, prob_ever_symptomatic) == 1
+    
+    # assign symptom onset days for the ever_symptomatic ones
+    if (this_agent_ever_symptomatic) {
+      
+      this_agent_symptom_delay <- round(runif(1,
+                                              min_symptom_delay,
+                                              max_symptom_delay))
+    } else {
+      this_agent_symptom_delay <- NA
+    }
+    # add to the new infected agents block
+    new_infected_agents$infection_day[this_infected_agent] <- day
+    new_infected_agents$ever_symptomatic[this_infected_agent] <- this_agent_ever_symptomatic
+    new_infected_agents$symptom_delay[this_infected_agent] <- this_agent_symptom_delay
+  }
+  # track these in the full list of agents
+  
+  # find the next block of empty spaces in the full tibble of agents
+  infected_agents_so_far <- sum(!is.na(agents$infection_day))
+  infected_agents_to_insert <- seq(infected_agents_so_far + 1, length.out = n_new_infections)
+  
+  # add the infection information them in this space
+  agents[infected_agents_to_insert, ] <- new_infected_agents
+  
+  # implement clinical case counting for this day
+  
+  # how many agents became symptomatic today?
+  day_of_symptom_onset <- agents$infection_day + agents$symptom_delay
+  n_new_cases_today <- sum(day_of_symptom_onset == day, na.rm = TRUE)
+  clinical_cases$cases[day] <- n_new_cases_today
+  
+  # implement prevalence survey on survey days  
+  is_prevalence_survey_day <- day %in% prev_days
+  if (is_prevalence_survey_day) {
+    
+    # sample agents at random from the population
+    which_agents_sampled <- sample(seq_len(pop), size = prev_n_sampled)
+    agents_sampled <- agents[which_agents_sampled, ]
+    
+    # tabulate which are positive
+    n_positive_this_survey <- 0
+    
+    # for each tested agent
+    for (this_tested_agent in seq_len(prev_n_sampled)) {
+      
+      # see when they were infected (if at all)
+      this_infection_day <- agents_sampled$infection_day[this_tested_agent]
+      
+      # have they been infected by now?
+      this_agent_infected_by_now <- !is.na(this_infection_day)
+      
+      if (this_agent_infected_by_now) {
+        
+        # if so, are they positive today?
+        this_tested_agent_days_positive <- this_infection_day +
+          seq_len(test_positive_duration)
+        this_agent_positive_today <- day %in% this_tested_agent_days_positive
+        
+        # if so, add them to the count        
+        if (this_agent_positive_today) {
+          n_positive_this_survey <- n_positive_this_survey + 1
+        }
+      }
+    }
+    # record the number tested and the number positive
+    prevalence_survey_round <- which(prevalence_surveys$day == day)
+    prevalence_surveys$n_sampled[prevalence_survey_round] <- prev_n_sampled
+    prevalence_surveys$n_positive[prevalence_survey_round] <- n_positive_this_survey
+  }
+}
+
+#### Outputs ####
+library(readr)
+# Case timeseries
+clinical_cases
+write_csv(x = clinical_cases, file = "outputs/case_timeseries.csv")
+
+#Prevalence survey timeseries
+prevalence_surveys
+write_csv(x = prevalence_surveys, file = "outputs/prevalence_timeseries.csv")
+
+#Let's come back to our plot of infections
+plot(inf_daily ~ days, type = "l", lty = "dotted", col = "grey50")
+
+# add the infections so we can compare
+lines(cases ~ days,
+      data = clinical_cases)
+
+#plot the positivity from the prevalence surveys
+#First we need to calculate the % positive
+prevalence_surveys$percentage_positive <- (prevalence_surveys$n_positive/prevalence_surveys$n_sampled)*100
+
+plot(x = prevalence_surveys$day,
+     y = prevalence_surveys$percentage_positive, 
+     type = "p")
 
 
 
